f7146b2073bdc0335d82aeef7cfdccd081a9298e
Steven <93779447+eplxy@users.noreply.github.com>
2026-01-15 23:03:26 -0500

    Add air and gitignore

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
-	_ "github.com/works-on-my-machine-390/concordia-waze/docs"
+	_ "works-on-my-machine/concordia-waze/docs"
+	"works-on-my-machine/concordia-waze/internal/service"
 )
 
 // @title Concordia Waze API
@@ -18,33 +17,19 @@ import (
 
 func main() {
 
-	router := gin.Default()
-	router.GET("/index", getGreetingString)
-	router.GET("/index/obj", getGreetingObject)
-
-	// Swagger route
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
+	router := setupRouter()
 	router.Run("localhost:8080")
 
 }
 
-// @Summary Get greeting string
-// @Description Returns a simple greeting string
-// @Tags greeting
-// @Produce plain
-// @Success 200 {string} string "hello world"
-// @Router /index [get]
-func getGreetingString(c *gin.Context) {
-	c.String(http.StatusOK, "hello world")
-}
+func setupRouter() *gin.Engine {
+	router := gin.Default()
 
-// @Summary Get greeting object
-// @Description Returns a greeting message as JSON object
-// @Tags greeting
-// @Produce json
-// @Success 200 {object} map[string]string
-// @Router /index/obj [get]
-func getGreetingObject(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "hello world"})
+	router.GET("/index", service.GetGreetingString)
+	router.GET("/index/obj", service.GetGreetingObject)
+
+	// Swagger route
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	return router
 }
