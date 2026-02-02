cf497db23c2324dbf292559eb4eb6aff65e77953
Manel Hellou <manelhellou0503@outlook.fr>
2026-02-02 12:23:10 -0500

    Added back the QueryClient and QueryClientProvider

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,18 +2,25 @@
 Navigation wrapper using Expo Router's Stack navigator that defines three routes (index, login, register) 
 */
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } },
+});
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "#f5f2f2" }
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="register" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "#f5f2f2" }
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
