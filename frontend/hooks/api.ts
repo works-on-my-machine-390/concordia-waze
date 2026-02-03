1b05b1ffd13de4f94cca7d5b8d829d023d9c5c3f
amal-1224 <amalsabsabi@icloud.com>
2026-02-02 22:58:07 -0500

    oops, you did not see my IP

--- frontend/hooks/api.ts
+++ frontend/hooks/api.ts
@@ -1,6 +1,6 @@
 import wretch from "wretch";
 
-export const API_URL = "http://10.0.0.156:8080";
+export const API_URL = process.env.REACT_APP_API_BASE || "http://localhost:8080";
 
 export const api = (token?: string) => 
     wretch(API_URL).headers(token ? {Authorization: `Bearer ${token}` } : {});
