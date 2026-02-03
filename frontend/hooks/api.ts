f7dae26401924257aa93ff380ea51ba5e95e9e3d
amal-1224 <amalsabsabi@icloud.com>
2026-02-03 18:17:53 -0500

    anything to remove env variables

--- frontend/hooks/api.ts
+++ frontend/hooks/api.ts
@@ -1,7 +1,18 @@
 import wretch from "wretch";
+import Constants from "expo-constants";
 
-export const API_URL = process.env.REACT_APP_API_BASE || "http://localhost:8080";
+const debuggerHost = Constants.expoConfig?.hostUri?.split(":").shift();
 
-export const api = (token?: string) => 
-    wretch(API_URL).headers(token ? {Authorization: `Bearer ${token}` } : {});
+const getBaseUrl = () => {
+  if (__DEV__ && debuggerHost) {
+    return `http://${debuggerHost}:8080`;
+  }
+  return "http://localhost:8080";
+};
+
+export const API_URL = getBaseUrl();
+
+
+export const api = (token?: string) =>
+  wretch(API_URL).headers(token ? { Authorization: `Bearer ${token}` } : {});
 
