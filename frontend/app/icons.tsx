b417098defd028b1d0dafee531bd7e39a83531ec
Manel Hellou <manelhellou0503@outlook.fr>
2026-02-05 15:26:07 -0500

    Added icons for login/logout

--- frontend/app/icons.tsx
+++ frontend/app/icons.tsx
@@ -37,3 +37,13 @@ export const EyeHidingIcon: React.FC<IconProps> = ({ size = DEFAULT_ICON_SIZE, c
 export const BackIcon: React.FC<IconProps> = ({ size = DEFAULT_ICON_SIZE, color = DEFAULT_ICON_COLOR }) => (
   <Ionicons name="arrow-back" size={size} color={color} />
 );
+
+// Logout icon 
+export const LogoutIcon: React.FC<IconProps> = ({ size = DEFAULT_ICON_SIZE, color = DEFAULT_ICON_COLOR }) => (
+  <MaterialIcons name="logout" size={size} color={color} />
+);
+
+// Login icon 
+export const LoginIcon: React.FC<IconProps> = ({ size = DEFAULT_ICON_SIZE, color = DEFAULT_ICON_COLOR }) => (
+  <MaterialIcons name="login" size={size} color={color} />
+);
