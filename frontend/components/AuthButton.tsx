59fee8752d82899d3e9242b281c4f0d268395458
Manel Hellou <manelhellou0503@outlook.fr>
2026-02-05 15:25:15 -0500

    Added menu auth button

--- frontend/components/AuthButton.tsx
+++ frontend/components/AuthButton.tsx
@@ -1,7 +1,8 @@
 /*
 Button component that displays a loading spinner when authentication is in progress
 */
-import { ActivityIndicator, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LoginIcon, LogoutIcon } from "../app/icons";
 import { colors } from "../app/styles/theme";
 
 export default function AuthButton({
@@ -9,20 +10,38 @@ export default function AuthButton({
   onPress,
   disabled,
   loading,
+  variant,
+  loggedIn,
 }: {
-  title: string;
+  title?: string;
   onPress?: () => void;
   disabled?: boolean;
   loading?: boolean;
+  variant?: "default" | "menu";
+  loggedIn?: boolean
 }) {
+  // Determining the title (Sign In/ Log Out) and icon for menu signin/logout buttons
+  const menuTitle = loggedIn ? "Log out" : "Login";
+  const icon = loggedIn ? <LogoutIcon /> : <LoginIcon />;
   return (
     <TouchableOpacity
       accessibilityRole="button"
       onPress={onPress}
       disabled={disabled || loading}
-      style={[styles.button, disabled ? styles.buttonDisabled : null]}
+      style={[
+        variant === "menu" ? styles.menuButton : styles.button,
+        disabled ? styles.buttonDisabled : null
+      ]}
     >
-      {loading ? <ActivityIndicator color="#fff" /> : <Text style={styles.text}>{title}</Text>}
+      {loading ? (<ActivityIndicator color="#fff" />) : 
+                  variant === "menu" ? (
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                      <View style={{ marginRight: 8 }}>{icon}</View>
+                      <Text style={styles.menuText}>{menuTitle}</Text>
+                    </View>
+                  ) : (
+                    <Text style={styles.text}>{title}</Text>
+                  )}
     </TouchableOpacity>
   );
 }
@@ -42,6 +61,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     width: "100%",
   },
+  menuButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   buttonDisabled: { opacity: 0.6 },
   text: { color: "#fff", fontWeight: "700" },
+  menuText: {
+    color: colors.text,
+    fontWeight: "700"
+  }
 });
