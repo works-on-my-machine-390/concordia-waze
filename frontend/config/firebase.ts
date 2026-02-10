f7f9267cd9d5b3c91491bb734d54383268142a19
amal-1224 <amalsabsabi@icloud.com>
2026-02-10 17:14:04 -0500

    #126: Adding the set up for firebase DB

---
+++ frontend/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9cgbiCDWlyDCdTc-XlQ6O8tehZ8j44PU",
+  authDomain: "concordia-waze.firebaseapp.com",
+  projectId: "concordia-waze",
+  storageBucket: "concordia-waze.firebasestorage.app",
+  messagingSenderId: "443355803050",
+  appId: "1:443355803050:web:6e730fd0c7957620c78fd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
