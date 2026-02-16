fb382070f18fcc24baa64a389791cceeb8473c10
catalexandre <catellier.alexandre@gmail.com>
2026-02-16 15:15:33 -0500

    Implemented metro_accessible flag retrieval in building repository

--- backend/internal/domain/building.go
+++ backend/internal/domain/building.go
@@ -1,15 +1,16 @@
 package domain
 
 type Building struct {
-	Code      string  `json:"code"`
-	Name      string  `json:"name"`
-	LongName  string  `json:"long_name"`
-	Address   string  `json:"address"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
+	Code            string  `json:"code"`
+	Name            string  `json:"name"`
+	LongName        string  `json:"long_name"`
+	Address         string  `json:"address"`
+	Latitude        float64 `json:"latitude"`
+	Longitude       float64 `json:"longitude"`
+	MetroAccessible bool    `json:"metro_accessible"`
 
-	Services      []string `json:"services"`
-	Departments   []string `json:"departments"`
-	Venues        []string `json:"venues"`
-	Accessibility []string `json:"accessibility"`
+	Services        []string `json:"services"`
+	Departments     []string `json:"departments"`
+	Venues          []string `json:"venues"`
+	Accessibility   []string `json:"accessibility"`
 }
