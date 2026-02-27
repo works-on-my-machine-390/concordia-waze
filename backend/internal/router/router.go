30bc16b4d58f47d8718a496c8f24fd90de8d8818
Steven <93779447+eplxy@users.noreply.github.com>
2026-02-27 11:53:35 -0500

    Add endpoint to retrieve shuttle marker positions

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -90,6 +90,7 @@ func SetupRouter() *gin.Engine {
 	{
 		shuttleGroup.GET("", shuttleHandler.GetDepartureData)
 		shuttleGroup.GET("/:day/:campus_code", shuttleHandler.GetCampusDaySchedule)
+		shuttleGroup.GET("/markers", shuttleHandler.GetShuttleMarkerPositions)
 	}
 
 	router.GET("/campuses/:campus/buildings", campusHandler.GetCampusBuildings)
