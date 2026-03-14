cc0e859938bb3231a9b27edb989349de5a067e8a
Victor B <145293037+VictorBruson@users.noreply.github.com>
2026-03-14 11:47:09 -0400

    parsing update

--- backend/internal/application/google/calendar_client.go
+++ backend/internal/application/google/calendar_client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/works-on-my-machine-390/concordia-waze/internal/domain"
 	"golang.org/x/oauth2"
@@ -40,10 +41,11 @@ func (c *CalendarClient) GetCalendarEvents(token *oauth2.Token) ([]string, error
 	fmt.Println(events, err)
 	for _, item := range events.Items {
 		parsedEvent, err2 := c.parseEvent(item)
-		if err2 != nil {
+		if parsedEvent == nil {
 			fmt.Printf("Failed to parse event: %v\n", err2)
 			continue
 		}
+
 	}
 
 	return nil, err
@@ -52,7 +54,15 @@ func (c *CalendarClient) GetCalendarEvents(token *oauth2.Token) ([]string, error
 func (c *CalendarClient) parseEvent(event *calendar.Event) (*domain.ClassItem, string) {
 
 	var classItem domain.ClassItem
+	id := event.Id
+	if event.RecurringEventId != "" {
+		id = event.RecurringEventId
+	}
+	classItem.EventID = id
+	classItem.Origin = "google"
 
+	// name matching
+	// name should be in format "AAAA 000 AAA" where A is a capital letter and 0 is a digit, e.g. "COMP 202 LEC"
 	name := event.Summary
 	re := regexp.MustCompile(`^\s*([A-Z]{4} \d{3}) ([A-Z]{3})$`)
 	matches := re.FindStringSubmatch(name)
@@ -62,12 +72,35 @@ func (c *CalendarClient) parseEvent(event *calendar.Event) (*domain.ClassItem, s
 	classItem.ClassID = matches[1]
 	classItem.Type = strings.ToLower(matches[2])
 
-	location := event.Location
-	startTime := event.Start.DateTime
-	endTime := event.End.DateTime
+	// date parsing
+	startTime, err := time.Parse(time.RFC3339, event.Start.DateTime)
+	if err != nil {
+		return nil, fmt.Sprintf("Error parsing date:", err)
+	}
 
-	fmt.Printf("Event: %s, Location: %s, Start: %s, End: %s\n ", name, location, startTime, endTime)
+	classItem.Day = startTime.Weekday().String()
+	classItem.StartTime = startTime.Format("15:04")
 
-	return &domain.ClassItem{}, ""
+	endTime, err := time.Parse(time.RFC3339, event.End.DateTime)
+	if err != nil {
+		return nil, fmt.Sprintf("Error parsing date:", err)
+	}
+	classItem.EndTime = endTime.Format("15:04")
+
+	// location parsing
+	re = regexp.MustCompile(`^(.+?)(?:\s-\s(.+?))?(?:\sRm\s(.+))?$`)
+	matches = re.FindStringSubmatch(event.Location)
+
+	if len(matches) == 2 {
+		// to do add building code from fuzz matching building name (create method in building repository that does this)
+		classItem.BuildingCode = matches[2]
+		return &classItem, fmt.Sprintf("Error could not find room number000")
+	} else if len(matches) == 3 {
+		classItem.BuildingCode = matches[1]
+		classItem.Room = matches[2]
+		return &classItem, ""
+	} else {
+		return &classItem, fmt.Sprintf("Error parsing location, does not match expected format: %s", event.Location)
+	}
 
 }
