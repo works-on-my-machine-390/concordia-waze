2c8e5a368e8b33dd16af8f501a06c2c01f8cbe9e
Victor B <145293037+VictorBruson@users.noreply.github.com>
2026-03-14 09:54:13 -0400

    pre merge dev commit

--- backend/internal/application/google/calendar_client.go
+++ backend/internal/application/google/calendar_client.go
@@ -3,6 +3,8 @@ package google
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/works-on-my-machine-390/concordia-waze/internal/domain"
 	"golang.org/x/oauth2"
@@ -37,20 +39,35 @@ func (c *CalendarClient) GetCalendarEvents(token *oauth2.Token) ([]string, error
 
 	fmt.Println(events, err)
 	for _, item := range events.Items {
-		c.parseEvent(item)
+		parsedEvent, err2 := c.parseEvent(item)
+		if err2 != nil {
+			fmt.Printf("Failed to parse event: %v\n", err2)
+			continue
+		}
 	}
 
 	return nil, err
 }
 
-func (c *CalendarClient) parseEvent(event *calendar.Event) *domain.ClassItem {
+func (c *CalendarClient) parseEvent(event *calendar.Event) (*domain.ClassItem, string) {
+
+	var classItem domain.ClassItem
+
 	name := event.Summary
+	re := regexp.MustCompile(`^\s*([A-Z]{4} \d{3}) ([A-Z]{3})$`)
+	matches := re.FindStringSubmatch(name)
+	if matches == nil {
+		return nil, fmt.Sprintf("event name does not match expected format AAAA 000 AAA: %s", name)
+	}
+	classItem.ClassID = matches[1]
+	classItem.Type = strings.ToLower(matches[2])
+
 	location := event.Location
 	startTime := event.Start.DateTime
 	endTime := event.End.DateTime
 
 	fmt.Printf("Event: %s, Location: %s, Start: %s, End: %s\n ", name, location, startTime, endTime)
 
-	return &domain.ClassItem{}
+	return &domain.ClassItem{}, ""
 
 }
