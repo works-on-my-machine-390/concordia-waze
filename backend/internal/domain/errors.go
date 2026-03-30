ae2d3698e4e515a30d43f6c1b2ee0b7670a67a0f
catalexandre <catellier.alexandre@gmail.com>
2026-03-30 17:01:08 -0400

    formatting

--- backend/internal/domain/errors.go
+++ backend/internal/domain/errors.go
@@ -22,13 +22,13 @@ var (
 	ErrInvalidMode = errors.New("Invalide mode, must be one of: walking, driving, transit, shuttle, bicycling")
 
 	// Favorite errors
-	ErrFavoriteNotFound     = errors.New("favorite not found")
-  ErrFavoriteAlreadyExists = errors.New("favorite already exists")
-	ErrEmptyFavoriteName    = errors.New("name cannot be empty")
-	ErrInvalidFavoriteType  = errors.New("type must be 'outdoor' or 'indoor'")
-	ErrOutdoorMissingCoords = errors.New("outdoor favorites require latitude and longitude")
-	ErrIndoorMissingFields  = errors.New("indoor favorites require buildingCode, floorNumber, x, and y")
-	ErrCourseNotFound       = errors.New("course not found")
+	ErrFavoriteNotFound      = errors.New("favorite not found")
+	ErrFavoriteAlreadyExists = errors.New("favorite already exists")
+	ErrEmptyFavoriteName     = errors.New("name cannot be empty")
+	ErrInvalidFavoriteType   = errors.New("type must be 'outdoor' or 'indoor'")
+	ErrOutdoorMissingCoords  = errors.New("outdoor favorites require latitude and longitude")
+	ErrIndoorMissingFields   = errors.New("indoor favorites require buildingCode, floorNumber, x, and y")
+	ErrCourseNotFound        = errors.New("course not found")
 
 	// Room Search errors
 	ErrRoomNotFound     = errors.New("room not found")
